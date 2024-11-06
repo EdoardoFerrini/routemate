fefcc6b43e73d1a320a3efa4cd68bfe2a4c1c17b
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-11-06 18:20:56 +0100

    add no route found

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Contact } from "./components/Contact";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Admin } from "./components/Admin";
+import { PageNotFound } from "./components/PageNotFound";
 
 function App() {
   const admin = true;
@@ -19,7 +20,8 @@ function App() {
           <Route path="/products" element={<ProductList />}></Route>
           <Route path="/products/:id" element={<ProductDetail />}></Route>
           <Route path="contact" element={<Contact />}></Route>
-          <Route path="/admin" element={admin ? <Admin /> : <Navigate to="/" />}></Route>
+          <Route path="/admin" element={admin ? <Admin /> : <PageNotFound />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
       </main>
 
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const PageNotFound = () => {
+  return (
+    <div className="component">
+      <p>404 Ops!</p>
+    </div>
+  )
+}
+
