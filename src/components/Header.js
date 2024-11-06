fb83bd50430de2c05ab4efe1840b7c6c2a2029ff
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-11-06 17:09:30 +0100

    bug fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,20 @@ import { Link, NavLink } from "react-router-dom";
 export const Header = () => {
   return (
     <header>
-      <Link href="/" className="logo">
+      <Link to="/" className="logo">
         <img src={Logo} alt="Routemate Logo"></img>
         <span>Route Mate</span>
       </Link>
       <nav className="navigation">
-        <NavLink to="/" className="link">Home</NavLink>
-        <NavLink to="/products" className="link">Products</NavLink>
-        <NavLink to="/contact" className="link">Contacts</NavLink>
+        <NavLink to="/" className="link">
+          Home
+        </NavLink>
+        <NavLink to="/products" className="link">
+          Products
+        </NavLink>
+        <NavLink to="/contact" className="link">
+          Contacts
+        </NavLink>
       </nav>
     </header>
   );
